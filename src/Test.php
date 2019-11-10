c1d6b8ce7f8620679958da9ddb7a77d26f31a249
Ondřej Maxa <ondrej.maxa.ih1@gmail.com>
2019-11-10 22:34:14 +0100

    Test class

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace MajFrame\Plugins;
+
+/**
+ * Description of Test
+ *
+ * @author Majksa
+ */
+class Test
+{
+    const AHOJ = "cau";
+}
